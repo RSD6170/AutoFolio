0e4a47a194b47e062322c02fc8357ed3b59833e2
RSD6170 <rsd6170+github@mailbox.org>
2023-06-27 20:16:55 +0200

    feat: modify test

--- examples/test_MAXSAT19_UCMS/example.py
+++ examples/test_MAXSAT19_UCMS/example.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 from autofolio.facade.af_csv_facade import AFCsvFacade
@@ -14,7 +16,7 @@ af = AFCsvFacade(scenario_path=scenario_path)
 af.fit()
 
 # tune AutoFolio's hyperparameter configuration for 20 iterations
-config = af.tune(runcount_limit=20)
+config = af.tune(runcount_limit=25)
 
 # evaluate configuration using a 10-fold cross validation
 score = af.cross_validation(config=config)
@@ -26,9 +28,18 @@ af.fit(config=config, save_fn=model_fn)
 # feature close_solutions/teams16_l6a.cnf.wcnf
 # best solver: UWrMaxSAT
 feature_vec = [2969,66540,2970,66539,-0.000337,0.000015,0.83,0.044635,0.384697,0.407561,0,1,1.025312,0.001204,0.364561,0.000673,0.005387,1.051858,0,0.043072,0.620914,0.05,0.001204,14.667463,0.00003,0.962368,1.655346,0.758216,0.327084,0,1,1.44021,0.001002,15.617237,0.00003,0.853019,1.775754,0.885015,0.000634,1.694881,0.00003,0.04462,0.13,-1,0,-1,0,-512,-1,0,-1,0,0,20.05]
+standard_deviation = 2
+
+mod_feature_vec = [random.normalvariate(0, standard_deviation)*x+x for x in feature_vec]
 
 # load AutoFolio model and
 # get predictions for new meta-feature vector
 pred = AFCsvFacade.load_and_predict(vec=feature_vec, load_fn=model_fn)
 
+pred_mod = AFCsvFacade.load_and_predict(vec=mod_feature_vec, load_fn=model_fn)
+
+print("Unmodified vector:")
 print(pred)
+print("---------------- \nModified vector:")
+print(mod_feature_vec)
+print(pred_mod)
