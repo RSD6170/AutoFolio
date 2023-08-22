d606cbc49a44f3bcba630d851e202550b859951c
RSD6170 <rsd6170+github@mailbox.org>
2023-08-22 14:59:23 +0200

    feat: add logging for incumbent analysis

--- scripts/evaluate_trajectory.py
+++ scripts/evaluate_trajectory.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 import ConfigSpace
 import numpy as np
@@ -7,11 +8,14 @@ from matplotlib import pyplot as plt
 from autofolio.facade.af_csv_facade import AFCsvFacade
 
 
-path_intensifier = "/tmp/pycharm_project_349/smac3_output/f06f4b0a069130b42ce8e8ec32c2cf94/12345/intensifier.json"
-path_runhistory = "/tmp/pycharm_project_349/smac3_output/f06f4b0a069130b42ce8e8ec32c2cf94/12345/runhistory.json"
-scenario_path = "/tmp/pycharm_project_349/examples/test_sharpSAT"
+path_intensifier = "/home/ubuntu/MCC22/MCC_T1_public_2000I/12345/intensifier.json"
+path_runhistory = "/home/ubuntu/MCC22/MCC_T1_public_2000I/12345/runhistory.json"
+scenario_path = "/home/ubuntu/AutoFolio/examples/MCC2022_Track1_public"
 
 af = AFCsvFacade(scenario_path=scenario_path, maximize=False)
+
+logging.getLogger("Stats").setLevel(logging.INFO)
+
 af.fit()
 
 values = {}
