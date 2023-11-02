c326030704573bed89d2362a310860057ae6a7d0
RSD6170 <rsd6170+github@mailbox.org>
2023-11-02 09:14:06 +0100

    feat: add second FM example with 30s feature extraction timeout

--- examples/Feature_Model_60sExtraction/example.py
+++ examples/Feature_Model_60sExtraction/example.py
@@ -5,7 +5,7 @@ import numpy as np
 from autofolio.facade.af_csv_facade import AFCsvFacade
 
 #path to data
-scenario_path = "examples/test_sharpSAT"
+scenario_path = "examples/Feature_Model_60sExtraction"
 
 # will be created (or overwritten) by AutoFolio
 model_fn = "af_model.pkl"
